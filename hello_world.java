6c3a1c132f291e42f80ac6991feec5d7317304da
Yerraballi Suresh Kumar Reddy <31470451+sureshreddy56@users.noreply.github.com>
2021-02-23 14:43:46 +0530

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java.java_learning;
+
+/**
+ *
+ * @author Suresh Reddy
+ */
+public class hello_world {
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+    
+}
